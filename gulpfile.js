da70df71abd3fa5ed0a9b1309fc35e2fb800d680
znsw007 <znsw007@163.com>
2017-08-31 11:11:39 +0800

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,180 @@
+var path = require('path');
+var fs = require('fs');
+var yargs = require('yargs').argv;
+var gulp = require('gulp');
+var less = require('gulp-less');
+var header = require('gulp-header');
+var cleancss = require('gulp-clean-css');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var bs = require('browser-sync').create('OREO_UI Server');
+var tap = require("gulp-tap");
+var option = {
+    base: 'src'
+};
+var pkg = require('./package.json');
+var config = {
+    src: {
+        main: 'src/style/oreo_ui.less',
+        example_style: 'src/example/res/css/example.less',
+        example_assets: 'src/example/**/*.?(png|jpg|gif|js)',
+        example_html: 'src/example/index.html',
+        example_components: 'src/example/components'
+    },
+    dist: __dirname + '/dist',
+    AUTOPREFIXER_BROWSERS: ["Android >= 4", "Explorer >= 10", "iOS >= 7"],
+    banner: [
+        '/*!',
+        ' * Oreo UI v<%= pkg.version %> (<%= pkg.homepage %>)',
+        ' * Copyright <%= new Date().getFullYear() %> Newcapec Mobile Internet, Inc.',
+        ' * Licensed under the <%= pkg.license %> license',
+        ' */',
+        ''
+    ].join('\n')
+};
+
+
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+        .filter(function (file) {
+            return fs.statSync(path.join(dir, file)).isFile();
+        });
+}
+
+
+
+
+gulp.task('build:style', function () {
+
+    gulp.src(config.src.main, option)
+        .pipe(sourcemaps.init())
+        .pipe(less().on('error', function (e) {
+            console.error(e.message);
+            this.emit('end')
+        }))
+        .pipe(postcss([autoprefixer(config.AUTOPREFIXER_BROWSERS)]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.dist))
+        .pipe(bs.reload({
+            stream: true
+        }))
+        .pipe(cleancss())
+        .pipe(rename(function (path) {
+            path.basename += '.min';
+        }))
+        .pipe(gulp.dest(config.dist));
+})
+
+gulp.task('build:example:assets', function () {
+    gulp.src(config.src.example_assets, option)
+        .pipe(gulp.dest(config.dist))
+        .pipe(bs.reload({
+            stream: true
+        }));
+})
+
+gulp.task('build:example:style', function () {
+    gulp.src(config.src.example_style, option)
+        .pipe(less().on('error', function (e) {
+            console.error(e.message);
+            this.emit('end');
+        }))
+        .pipe(postcss([autoprefixer(config.AUTOPREFIXER_BROWSERS)]))
+        .pipe(cleancss())
+        .pipe(gulp.dest(config.dist))
+        .pipe(bs.reload({
+            stream: true
+        }));
+})
+
+gulp.task('build:example:components', function () {
+    gulp.src(config.src.example_html, option)
+        .pipe(tap(function (file) {
+
+            var dir = path.dirname(file.path);
+            var contents = file.contents.toString();
+
+            var src = './src/example/components'
+            var cps = getFolders(src);
+            var tpls = [];
+            cps.forEach(function (v, index) {
+                var filename = path.join(src, v);
+                var id = path.basename(filename, '.html');
+
+                var content = fs.readFileSync(filename, 'utf-8');
+                tpls.push('<script type="text/html" id="tpl_' + id + '">\n' + content + '\n</script>')
+            })
+
+            contents = contents.replace(/<script\s+type="text\/html"\s*><\/script>/gi, tpls.join(''));
+
+            file.contents = new Buffer(contents);
+        }))
+        .pipe(gulp.dest(config.dist))
+        .pipe(bs.reload({
+            stream: true
+        }));
+});
+
+gulp.task('build:example:html', function () {
+    gulp.src(config.src.example_html, option)
+        .pipe(tap(function (file) {
+            var contents = file.contents.toString();
+
+            var cps = getFolders(config.src.example_components);
+            var tpls = [];
+            cps.forEach(function (v, index) {
+                var filename = path.join(config.src.example_components, v);
+                var id = path.basename(filename, '.html');
+
+                var content = fs.readFileSync(filename, 'utf-8');
+                tpls.push('<script type="text/html" id="tpl_' + id + '">\n' + content + '\n</script>\n')
+            })
+            contents = contents.replace(/<script\s+type="text\/html"\s*><\/script>/gi, tpls.join(''));
+            file.contents = new Buffer(contents);
+        }))
+        .pipe(gulp.dest(config.dist))
+        .pipe(bs.reload({
+            stream: true
+        }));
+})
+
+gulp.task('build:example', ['build:example:assets', 'build:example:style', 'build:example:html'])
+
+gulp.task('release', ['build:style', 'build:example'])
+
+gulp.task('watch', ['release'], function () {
+    gulp.watch('src/style/**/*', ['build:style']);
+    gulp.watch('src/example/res/css/example.less', ['build:style']);
+    gulp.watch('src/example/**/*.?(png|jpg|gif|js)', ['build:example:assets']);
+    gulp.watch('src/**/*.html', ['build:example:html']);
+})
+
+gulp.task('server', function () {
+    yargs.p = yargs.p || 8086;
+    bs.init({
+        server: {
+            baseDir: './dist'
+        },
+        ui: {
+            port: yargs.p + 1,
+            weinre: {
+                port: yargs.p + 2
+            }
+        },
+        port: yargs.p,
+        startPath: '/example'
+    })
+})
+
+gulp.task('default', ['release'], function () {
+
+    if (yargs.s) {
+        gulp.start('server');
+    }
+
+    if (yargs.w) {
+        gulp.start('watch');
+    }
+})
